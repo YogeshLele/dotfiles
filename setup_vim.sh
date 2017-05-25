6e49ecb48e53887f399bacf4bf190439907f39ac
Yogesh Lele <lele.yogesh@gmail.com>
2017-05-24 22:22:02 -0500

    update setup_vim.sh

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 #backup existing ~/.vimrc to ~/.vimrc_bkp
+printf "\nBacking up previous vimrc to ~/.vimrc_bkp"
 mkdir -p ~/.vim_bkp
-cd ~/.vim_bkp
-cp ~/.vimrc .
+cp ~/.vimrc ~/.vim_bkp/
 
 git --version 2>&1 >/dev/null
 GIT_IS_AVAILABLE=$?
 if [ $GIT_IS_AVAILABLE -ne 0 ]; then
-    printf "\nInstalling git....."
+    printf "\nGit not installed. Installing git....."
     sudo apt-get install -y git
 fi
 
+git config --global credential.helper "cache --timeout=36000000"
+
 #get(git) my repo
-#git clone https://github.com/
-printf "Installing molokai theme colors..."
+printf "\nInstalling Vundle...."
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-mkdir -p /tmp/vim_install_temp
-git clone https://github.com/tomasr/molokai.git /tmp/vim_install_temp
-mkdir -p ~/.vim/colors && cp /tmp/vim_install_temp/colors/molokai.vim ~/.vim/colors/
-printf "Added molokai to ~/.vim/colors"
 
 #run the installation script
+printf "\nInstalling plugins...."
+cp vimrc ~/.vimrc
+vim +PluginInstall +qall
+
+printf "\nInstallation done....\n"
